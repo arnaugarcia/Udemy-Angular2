2e836e0ac898eb2f6771ab28a022b251903b4247
Arnau García <arnau.garcia.gallego@gmail.com>
2017-02-22 15:45:35 +0100

    Clase 18 - Modelo de datos

--- app/app.components.ts
+++ app/app.components.ts
@@ -2,6 +2,7 @@
  * Created by Arnau on 21/02/17.
  */
 import {Component} from "angular2/core";
+import {Pelicula} from "./model/pelicula";
 
 @Component({
     selector: "mi-app",
@@ -9,19 +10,15 @@ import {Component} from "angular2/core";
 })
 
 export class AppComponent{
-    public titulo:string = "Películas con Angular 2";
-    public pelicula:string;
-    public director:string;
-    public anio:number;
+
+    public pelicula:Pelicula;
 
     constructor(){
-        this.pelicula = "Batman v Superman";
-        this.director = "Zack Snider";
-        this.anio = 2016;
-        this.holaMundo();
+        this.pelicula = new Pelicula(1,"Batman v Superman","Zack Snider",2016);
+        this.debug();
     }
 
-    holaMundo(){
-        alert("Películas: " + this.pelicula + " - " + this.director + " - " + this.anio );
+    debug(){
+        console.log(this.pelicula);
     }
 }
